b9754434af5ea0772713a14156254e75be2aa2fc
Syed Basheeruddin Ahmed <sbua2007@gmail.com>
2017-11-21 13:40:01 -0800

    Arrays, inheritance,overloading, callbyreference
    Signed-off-by: Syed Basheeruddin Ahmed <sbua2007@gmail.com>

---
+++ beginners/samples/src/main/java/com/bitlrn/intro/inheritance/Account.java
@@ -0,0 +1,48 @@
+
+package com.bitlrn.intro.inheritance;
+
+
+abstract public class Account {
+    protected final long accountId;
+    protected final String name;
+    protected double balance;
+
+    protected Account(long accountId, String name, double initBalance) {
+        this.accountId = accountId;
+        this.name = name;
+        this.balance = initBalance;
+    }
+
+    public long getAccountId() {
+        return accountId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void deposit(double amount){
+        if(amount < 0){
+            System.out.println("Please deposit an amount > 0");
+        }
+        balance += amount;
+        System.out.println(amount + " deposited to account with id "+ this.accountId);
+    }
+
+    public void withdraw(double amount){
+        if(amount < 0){
+            System.out.println("Please withdraw amount > 0");
+        }
+        balance -= amount;
+        System.out.println(amount + " withdraw from account with id "+ this.accountId);
+    }
+
+    public void displayBalance (){
+        System.out.println("The balance in account with id "+ accountId + " with name "+name
+                            + " is "+ balance);
+    }
+}
